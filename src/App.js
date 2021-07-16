a0c94d3e5c3c25dc85f24181a25446257c5e9613
jessejohns-1 <84858554+jessejohns-1@users.noreply.github.com>
2021-07-16 14:11:14 -0400

    adding my characters content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import { MySecrets } from './components/Character';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+
+
+
+
 
 const App = () => {
+
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [data, setData] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`${MySecrets}`)
+    .then(res => {
+      setData(res.data)
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}, [])
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header"></h1>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
+import React from "react"
+import styled from 'styled-components'
 // Write your Character component here
+export const MySecrets = "https://swapi.dev/api/people/"
+//importing
+
+//creating my character card styles
+
+
+
+
+
+
+
+//creating my character card
+
+// const Character = ({ characters }} =>
+    
+
+
